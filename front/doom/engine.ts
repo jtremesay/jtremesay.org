a3932d3d045f352db4cb34866c07ae9f481628e2
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-02-06 23:42:40 +0100

    remove unused impert

--- front/doom/engine.ts
+++ front/doom/engine.ts
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
-import { WAD, Node, SubSector, SideDef, Sector } from "./types"
+import { WAD, Node, SubSector, Sector } from "./types"
 import * as THREE from "three"
 import * as d3 from "d3"
 
