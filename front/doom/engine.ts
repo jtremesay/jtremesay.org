d04134b96b41d980fbbe2511bd34200631ccf41f
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-02-08 19:50:54 +0100

    prepare for 3D !

--- front/doom/engine.ts
+++ front/doom/engine.ts
@@ -16,74 +16,19 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
-import { WAD, Sector } from "./types"
+import { WAD, Level } from "./types"
 import * as THREE from "three"
 import * as d3 from "d3"
-
-function generate_sector_group(sector: Sector): THREE.Group {
-    let paths = []
-    let side_defs = [...sector.side_defs]
-    let sd = side_defs.shift()!
-    paths.push([sd.line_def.start, sd.line_def.end])
-
-    while (side_defs.length) {
-        let last_path = paths[paths.length - 1]
-        let last_p = last_path[last_path.length - 1]
-
-        let sd_i = 0
-        for (; sd_i < side_defs.length; ++sd_i) {
-            let sd = side_defs[sd_i]
-            if (last_p.equals(sd.line_def.start)) {
-                last_path.push(sd.line_def.start)
-                last_path.push(sd.line_def.end)
-                break
-            } else if (last_p.equals(sd.line_def.end)) {
-                last_path.push(sd.line_def.end)
-                last_path.push(sd.line_def.start)
-                break
-            }
-        }
-
-        if (sd_i < side_defs.length) {
-            side_defs.splice(sd_i, 1)
-        } else {
-            let sd = side_defs.shift()!
-            paths.push([sd.line_def.start, sd.line_def.end])
-        }
-    }
-
-    let material = new THREE.MeshBasicMaterial({ color: `hsl(${Math.random() * 360}, 100%, 20%)` })
-    let material1 = new THREE.MeshBasicMaterial({ color: THREE.Color.NAMES.white })
-    let g = new THREE.Group()
-    for (let path of paths) {
-        let shape = new THREE.Shape(path)
-
-        g.add(new THREE.Mesh(new THREE.ShapeGeometry(shape), material))
-        g.add(new THREE.Line(new THREE.BufferGeometry().setFromPoints(shape.getPoints()), material1))
-    }
-
-    return g
-}
-
-function generate_sectors_group(sectors: Sector[], mask = -1): THREE.Group {
-    let g = new THREE.Group()
-    for (let i = 0; i < sectors.length; ++i) {
-        if (mask == -1 || i == mask) {
-            g.add(generate_sector_group(sectors[i]))
-        }
-    }
-    return g
-}
+import { View2D } from "./view2d"
+import { View3D } from "./view3d"
 
 export class Engine {
     wad: WAD
-    level_i: number = 0
     camera: THREE.Camera = new THREE.OrthographicCamera()
-    renderer: THREE.Renderer
     scene: THREE.Scene = new THREE.Scene()
     level_selector: HTMLSelectElement
-    draw_sectors_selector_cb: HTMLInputElement
-
+    view2D: View2D
+    view3D: View3D
 
     constructor(root: HTMLElement, wad: WAD) {
         this.wad = wad
@@ -94,7 +39,7 @@ export class Engine {
             .append("label")
             .text("Level:")
             .append("select")
-            .on("change", this.reload.bind(this))
+            .on("change", this.on_level_selected.bind(this))
         $level_selector
             .selectAll("option")
             .data(this.wad.levels)
@@ -103,39 +48,30 @@ export class Engine {
             .attr("label", (d) => d.name)
         this.level_selector = $level_selector.node()!
 
-        this.draw_sectors_selector_cb = $root.append("div")
-            .append("label")
-            .text("Sector:")
-            .append("input")
-            .attr("type", "number")
-            .attr("min", -1)
-            .attr("value", -1)
-            .on("input", this.reload.bind(this))
-            .node()!
-        this.renderer = new THREE.WebGLRenderer()
-        this.renderer.setSize(800, 600)
-        root.appendChild(this.renderer.domElement)
+        this.view2D = new View2D()
+        root.appendChild(this.view2D.renderer.domElement)
 
-        this.reload()
+        this.view3D = new View3D()
+        root.appendChild(this.view3D.renderer.domElement)
+
+        this.on_level_selected()
     }
 
-    reload() {
-        this.level_i = d3.select(this.level_selector).property("value")
-        let level = this.wad.levels[this.level_i]
-        let bb = level.root.bb.expandByScalar(100)
-        this.camera = new THREE.OrthographicCamera(bb.min.x, bb.max.x, bb.max.y, bb.min.y)
-        this.camera.position.z = 1;
+    load_level(level: Level) {
+        console.log(level)
+        this.view2D.load_level(level)
+        this.view3D.load_level(level)
+    }
 
-        this.scene = new THREE.Scene()
-
-        let $draw_sectors_selector = d3.select(this.draw_sectors_selector_cb)
-        $draw_sectors_selector.attr("max", level.sectors.length - 1)
-        let selected_sector_i = $draw_sectors_selector.property("value")
-        this.scene.add(generate_sectors_group(level.sectors, selected_sector_i))
+    on_level_selected() {
+        let level_i = d3.select(this.level_selector).property("value")
+        let level = this.wad.levels[level_i]
+        this.load_level(level)
     }
 
     draw() {
-        this.renderer.render(this.scene, this.camera)
+        this.view2D.render()
+        this.view3D.render()
     }
 
     run() {
