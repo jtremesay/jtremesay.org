5414fca46104fdc4e7b132e2a756fd7015b1ac6c
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-05-11 16:52:50 +0200

    add index page

--- jtremesay/urls.py
+++ jtremesay/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
 app_name = "jtremesay"
-urlpatterns = []
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="jtremesay/index.html"), name="index"),
+]
