94242e8592696bf7cb01b34ddabe301d5ff2db75
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-05-11 17:02:41 +0200

    vanilla js

--- jtremesay/urls.py
+++ jtremesay/urls.py
@@ -4,4 +4,9 @@ from django.views.generic import TemplateView
 app_name = "jtremesay"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="jtremesay/index.html"), name="index"),
+    path(
+        "js_sandbox",
+        TemplateView.as_view(template_name="jtremesay/js_sandbox.html"),
+        name="js_sandbox",
+    ),
 ]
