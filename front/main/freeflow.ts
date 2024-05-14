c6753396092acac8ecb1fab52f343fe93d6ec97d
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-05-15 00:26:04 +0200

    playable

--- front/main/freeflow.ts
+++ front/main/freeflow.ts
@@ -59,6 +59,26 @@ export class State {
         this.size = size;
         this.pills = pills;
     }
+
+    pill_at(position: Vec2): Pill | null {
+        for (let pill of this.pills) {
+            if (pill.position.x === position.x && pill.position.y === position.y) {
+                return pill;
+            }
+        }
+        return null;
+    }
+
+    flow_at(position: Vec2): Flow | null {
+        for (let flow of this.flows) {
+            for (let flow_pos of flow.positions) {
+                if (flow_pos.x === position.x && flow_pos.y === position.y) {
+                    return flow;
+                }
+            }
+        }
+        return null;
+    }
 }
 
 let state = new State(vec2(5, 4), [[
@@ -76,129 +96,133 @@ let state = new State(vec2(5, 4), [[
 ]].map((poss, i) => poss.map(pos => new Pill(i, pos))).flat());
 
 
-state.flows.push(new Flow(0, [
-    vec2(0, 0),
-    vec2(0, 1),
-    vec2(0, 2),
-    vec2(0, 3),
-    vec2(1, 3),
-    vec2(2, 3),
-    vec2(3, 3),
-]));
-// state.flows.push(new Flow(1, [
-//     vec2(1, 0),
-//     vec2(2, 0),
-//     vec2(3, 0),
-//     vec2(4, 0),
-//     vec2(4, 1),
-//     vec2(4, 2),
-//     vec2(4, 3),
-// ]));
-state.flows.push(new Flow(2, [
-    vec2(1, 2),
-    vec2(1, 1),
-    vec2(2, 1),
-]));
-// state.flows.push(new Flow(3, [
-//     vec2(2, 2),
-//     vec2(3, 2),
-//     vec2(3, 1),
-// ]));
-
-state.building_flow = new Flow(1, [
-    vec2(1, 0),
-    vec2(2, 0),
-    vec2(3, 0),
-    vec2(4, 0),
-    vec2(4, 1),
-]);
-
-
 let $svg = d3.select('#freeflow').append('svg');
-$svg.style('background-color', 'black');
-$svg.attr('width', `${state.size.x * tile_size_px}`);
-$svg.attr('height', `${state.size.y * tile_size_px}`);
 
-// draw tiles
-$svg.append('g')
-    .classed('tiles', true)
-    .selectAll("path")
-    .data(state.flows)
-    .join('path')
-    .attr('d', flow => {
-        return flow.positions.map((pos, i) => {
-            return `${i === 0 ? 'M' : 'L'} ${pos.x * tile_size_px + tile_size_px / 2} ${pos.y * tile_size_px + tile_size_px / 2}`;
-        }).join(' ');
-    })
-    .attr('stroke', flow => hsla(HUES[flow.match_id], 9, 0.2))
-    .attr('stroke-width', tile_size_px)
-    .attr("stroke-linejoin", "square")
-    .attr("stroke-linecap", "square")
-    .attr('fill', 'none');
+function init(svg: SVGElement, state: State) {
+    let $svg = d3.select(svg);
+    $svg.style('background-color', 'black');
+    $svg.attr('width', `${state.size.x * tile_size_px}`);
+    $svg.attr('height', `${state.size.y * tile_size_px}`);
 
-// Draw grid
-$svg.append('g')
-    .classed('grid', true)
-    .call($grid => {
+    // draw tiles
+    $svg.append('g').classed('tiles', true)
 
-        $grid.append("g")
-            .classed("hlines", true)
-            .selectAll('line')
-            .data(Array(state.size.y + 1).fill(null))
-            .join('line')
-            .attr('x1', 0)
-            .attr('x2', state.size.x * tile_size_px)
-            .attr('y1', (_, i) => i * tile_size_px)
-            .attr('y2', (_, i) => i * tile_size_px)
-            .attr('stroke', 'white');
-        $grid.append("g")
-            .classed("vlines", true)
-            .selectAll('line')
-            .data(Array(state.size.x + 1).fill(null))
-            .join('line')
-            .classed('vline', true)
-            .attr('y1', 0)
-            .attr('y2', state.size.y * tile_size_px)
-            .attr('x1', (_, i) => i * tile_size_px)
-            .attr('x2', (_, i) => i * tile_size_px)
-            .attr('stroke', 'white');
+    // Draw grid
+    $svg.append('g')
+        .classed('grid', true)
+        .selectAll("rect")
+        .data(Array(state.size.x * state.size.y).fill(null).map((_, i) => vec2(i % state.size.x, Math.floor(i / state.size.x))))
+        .join('rect')
+        .attr('x', (_, i) => (i % state.size.x) * tile_size_px)
+        .attr('y', (_, i) => Math.floor(i / state.size.x) * tile_size_px)
+        .attr('width', tile_size_px)
+        .attr('height', tile_size_px)
+        .attr('fill', 'none')
+        .attr('stroke', 'white')
+        .attr('stroke-width', 1)
+
+    // draw flows
+    $svg.append('g').classed('flows', true);
+
+    // draw pills
+    $svg.append('g')
+        .classed('pills', true)
+        .selectAll('circle')
+        .data(state.pills)
+        .join('circle')
+        .attr('cx', pill => pill.position.x * tile_size_px + tile_size_px / 2)
+        .attr('cy', pill => pill.position.y * tile_size_px + tile_size_px / 2)
+        .attr('r', tile_size_px / 4)
+        .attr('fill', pill => hsla(HUES[pill.match_id], 1, 0.5))
+        .on("mouseover", function () {
+            d3.select(this).transition()
+                .duration(1000)
+                .ease(d3.easeElastic)
+                .attr('r', tile_size_px / 3)
+        }).on("mouseout", function () {
+            d3.select(this).transition()
+                .duration(1000)
+                .ease(d3.easeElastic)
+                .attr('r', tile_size_px / 4)
+        })
+
+
+    $svg.on('click', (event) => {
+        let coords = vec2(Math.floor(event.offsetX / tile_size_px), Math.floor(event.offsetY / tile_size_px));
+        let pill = state.pill_at(coords);
+        if (pill !== null) {
+            state.building_flow = new Flow(pill.match_id, [pill.position]);
+            state.flows = state.flows.filter(flow => flow.match_id !== pill.match_id);
+            draw(svg, state);
+        } else {
+            let flow = state.flow_at(coords);
+            if (flow !== null) {
+                state.flows = state.flows.filter(f => f !== flow);
+                draw(svg, state);
+            }
+        }
     });
 
-// draw pills
-$svg.append('g')
-    .classed('pills', true)
-    .selectAll('circle')
-    .data(state.pills)
-    .join('circle')
-    .attr('cx', pill => pill.position.x * tile_size_px + tile_size_px / 2)
-    .attr('cy', pill => pill.position.y * tile_size_px + tile_size_px / 2)
-    .attr('r', tile_size_px / 4)
-    .attr('fill', pill => hsla(HUES[pill.match_id], 1, 0.5))
-    .on("mouseover", function () {
-        d3.select(this).transition()
-            .duration(1000)
-            .ease(d3.easeElastic)
-            .attr('r', tile_size_px / 2.5)
-    }).on("mouseout", function () {
-        d3.select(this).transition()
-            .duration(1000)
-            .ease(d3.easeElastic)
-            .attr('r', tile_size_px / 4)
-    })
+    $svg.on("mousemove", function (event) {
+        let coords = vec2(Math.floor(event.offsetX / tile_size_px), Math.floor(event.offsetY / tile_size_px));
+        if (state.building_flow !== null) {
+            let last_pos = state.building_flow.positions[state.building_flow.positions.length - 1];
+            let dx = coords.x - last_pos.x;
+            let dy = coords.y - last_pos.y;
+            if (Math.abs(dx) + Math.abs(dy) === 1 && state.flow_at(coords) === null) {
+                let pill = state.pill_at(coords);
+                if (pill === null || pill.match_id === state.building_flow.match_id) {
+                    state.building_flow.positions.push(coords);
 
-// draw flows
-$svg.append('g')
-    .classed('flows', true)
-    .selectAll("path")
-    .data([...state.flows, state.building_flow])
-    .join('path')
-    .attr('d', flow => {
-        return flow.positions.map((pos, i) => {
-            return `${i === 0 ? 'M' : 'L'} ${pos.x * tile_size_px + tile_size_px / 2} ${pos.y * tile_size_px + tile_size_px / 2}`;
-        }).join(' ');
-    })
-    .attr('stroke', flow => hsla(HUES[flow.match_id], 1, 0.5))
-    .attr('stroke-width', tile_size_px / 4)
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr('fill', 'none');
+                    if (pill !== null) {
+                        state.flows.push(state.building_flow);
+                        state.building_flow = null;
+                    }
+
+                }
+
+                draw(svg, state);
+            }
+        }
+    });
+}
+
+
+function draw(svg: SVGElement, state: State) {
+    let $svg = d3.select(svg);
+
+    // draw tiles
+    $svg.select('.tiles')
+        .selectAll("path")
+        .data(state.flows)
+        .join('path')
+        .attr('d', flow => {
+            return (flow.positions.length == 1 ? [flow.positions[0], flow.positions[0]] : flow.positions).map((pos, i) => {
+                return `${i === 0 ? 'M' : 'L'} ${pos.x * tile_size_px + tile_size_px / 2} ${pos.y * tile_size_px + tile_size_px / 2}`;
+            }).join(' ');
+        })
+        .attr('stroke', flow => hsla(HUES[flow.match_id], 9, 0.2))
+        .attr('stroke-width', tile_size_px)
+        .attr("stroke-linejoin", "square")
+        .attr("stroke-linecap", "square")
+        .attr('fill', 'none');
+
+    // draw flows
+    $svg.select('.flows')
+        .selectAll("path")
+        .data(state.flows.concat(state.building_flow || []))
+        .join('path')
+        .attr('d', flow => {
+            return (flow.positions.length == 1 ? [flow.positions[0], flow.positions[0]] : flow.positions).map((pos, i) => {
+                return `${i === 0 ? 'M' : 'L'} ${pos.x * tile_size_px + tile_size_px / 2} ${pos.y * tile_size_px + tile_size_px / 2}`;
+            }).join(' ');
+        })
+        .attr('stroke', flow => hsla(HUES[flow.match_id], 1, 0.5))
+        .attr('stroke-width', tile_size_px / 4)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr('fill', 'none');
+
+}
+init($svg.node()!, state);
+draw($svg.node()!, state);
