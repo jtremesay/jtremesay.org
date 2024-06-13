664a258469bb857258f68cee15e8eb28667fd7ea
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-06-13 21:37:25 +0200

    reformat

--- jtremesay/apps.py
+++ jtremesay/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class JtremesayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jtremesay'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jtremesay"
