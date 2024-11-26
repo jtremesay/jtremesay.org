42059b299235c05392754e08d08344e15acd134b
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-11-26 20:26:04 +0100

    can choose text

--- front/main/textnodes.ts
+++ front/main/textnodes.ts
@@ -20,85 +20,81 @@
 import 'vite/modulepreload-polyfill';
 import * as d3 from "d3";
 
-
-
-function generate_pixmap(text: string): [number, number, boolean[][]] {
-    const canvas = document.createElement("canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d")!;
-
-    // Get the size of the bounding box
-    ctx.font = "30px Arial";
-    const textMetrics = ctx.measureText(text);
-    const width = Math.ceil(textMetrics.width);
-    const height = Math.ceil(textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent);
-
-    // Resize the canvas to the bounding box
-    canvas.width = width;
-    canvas.height = height;
-
-    // Draw the text
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(text, width / 2, height / 2);
-
-    // Read the pixels
-    const imageData = ctx.getImageData(0, 0, width, height);
-    const pixels_data = imageData.data;
-    const pixmap: boolean[][] = [];
-    for (let y = 0; y < height; y++) {
-        pixmap[y] = [];
-        for (let x = 0; x < width; x++) {
-            const index = (y * width + x) * 4;
-            const r = pixels_data[index];
-            const g = pixels_data[index + 1];
-            const b = pixels_data[index + 2];
-            const a = pixels_data[index + 3];
-            // Fun fact: firefox use the alpha channel
-            // instead of the rgb channels to render the text
-            // so basically, all pixels are (0, 0, 0, a)
-            // where a is the alpha value of the pixel
-            // No idea if this tricks is used by the other browsers.
-            // So we just filter on any non null channel value
-            const val = Math.max(r, g, b, a);
-            pixmap[y][x] = val > 0;
-        }
-    }
-
-    return [width, height, pixmap];
-}
-
-function generate_pixlist(pixmap: boolean[][]): [number, number][] {
-    const pixlist: [number, number][] = [];
-    for (let y = 0; y < pixmap.length; y++) {
-        for (let x = 0; x < pixmap[y].length; x++) {
-            if (pixmap[y][x]) {
-                pixlist.push([x, y]);
-            }
-        }
-    }
-
-    return pixlist;
-}
-
 function main() {
     const text = "Hello, world!";
-    const [width, height, pixmap] = generate_pixmap(text);
-    const pixlist = generate_pixlist(pixmap);
-    console.log(pixlist);
+
+    const $main = d3.select("main");
+    const $input = $main.append("input")
+        .attr("type", "text")
+        .attr("value", "Hello, world!");
+
+    const $headless_canvas = d3.create("canvas")
+    const ctx = $headless_canvas.node()!.getContext("2d")!;
+
 
     const $svg = d3.select("main").append("svg")
-        .attr("width", width * 10)
-        .attr("height", height * 10);
-    $svg.selectAll("circle")
-        .data(pixlist)
-        .join("circle")
-        .attr("cx", d => d[0] * 10 + 5)
-        .attr("cy", d => d[1] * 10 + 5)
-        .attr("r", 5)
-        .attr("fill", "black");
+    function update(text: string) {
 
+        // Get the size of the bounding box
+        ctx.font = "30px Arial";
+        const textMetrics = ctx.measureText(text);
+        const width = Math.ceil(textMetrics.width);
+        const height = Math.ceil(textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent);
+
+        // Resize the canvas to the bounding box
+        $headless_canvas
+            .property("width", width)
+            .property("height", height);
+        // Draw the text
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(text, width / 2, height / 2);
+
+        // Read the pixels
+        const imageData = ctx.getImageData(0, 0, width, height);
+        const pixels_data = imageData.data;
+        const pixlist = [];
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const index = (y * width + x) * 4;
+                const r = pixels_data[index];
+                const g = pixels_data[index + 1];
+                const b = pixels_data[index + 2];
+                const a = pixels_data[index + 3];
+                // Fun fact: firefox use the alpha channel
+                // instead of the rgb channels to render the text
+                // so basically, all pixels are (0, 0, 0, a)
+                // where a is the alpha value of the pixel
+                // No idea if this tricks is used by the other browsers.
+                // So we just filter on any non null channel value
+                const val = Math.max(r, g, b, a);
+                if (val > 0) {
+                    pixlist.push([x, y]);
+                }
+            }
+        }
+
+        $svg
+            .attr("width", width * 10)
+            .attr("height", height * 10);
+        $svg.selectAll("circle")
+            .data(pixlist)
+            .join("circle")
+            .attr("cx", d => d[0] * 10 + 5)
+            .attr("cy", d => d[1] * 10 + 5)
+            .attr("r", 5)
+            .attr("fill", "black");
+    }
+
+
+    $input.on("input", function () {
+        const text = this.value;
+        update(text);
+    });
+
+    update(text)
 }
 
 
