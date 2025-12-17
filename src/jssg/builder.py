8322974a3a57c0a5584ea7f59e9ba1804dcadede
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-12-17 03:51:57 +0100

    refactor: update build functions to accept Jinja2 environment and improve template rendering style: clean up HTML templates by removing unnecessary template tags and adjusting paths new: add __init__.py to templates directory

--- src/jssg/builder.py
+++ src/jssg/builder.py
@@ -1,40 +1,51 @@
 import logging
 from pathlib import Path
 
+from jinja2 import Environment
+
 from jssg.models import Page, Post, Site
 
 logger = logging.getLogger(__name__)
 
-def build_page(page: Page, site: Site, dist_dir: Path) -> None:
+
+def build_page(page: Page, site: Site, dist_dir: Path, env: Environment) -> None:
     logger.info("Building page: %s", page.title)
+
+    tpl = env.get_template("page.html")
+    r = tpl.render(object=page, site=site)
+
     dist_dir.mkdir(parents=True, exist_ok=True)
     with open(dist_dir / f"{page.slug}.html", "w") as f:
-        pass
+        f.write(r)
 
-def build_post(post: Post, site: Site, dist_dir: Path) -> None:
+
+def build_post(post: Post, site: Site, dist_dir: Path, env: Environment) -> None:
     logger.info("Building post: %s", post.title)
     dist_dir.mkdir(parents=True, exist_ok=True)
     with open(dist_dir / f"{post.slug}.html", "w") as f:
         pass
 
-def build_atom_feed(site: Site, dist_dir: Path) -> None:
+
+def build_atom_feed(site: Site, dist_dir: Path, env: Environment) -> None:
     logger.info("Building Atom feed")
     dist_dir.mkdir(parents=True, exist_ok=True)
     with open(dist_dir / "atom.xml", "w") as f:
         pass
 
-def build_pages(site: Site, dist_dir: Path) -> None:
-    for page in site.pages:
-        build_page(page, site, dist_dir)
 
-def build_posts(site: Site, dist_dir: Path) -> None:
+def build_pages(site: Site, dist_dir: Path, env: Environment) -> None:
+    for page in site.pages:
+        build_page(page, site, dist_dir, env)
+
+
+def build_posts(site: Site, dist_dir: Path, env: Environment) -> None:
     dist_dir = dist_dir / "posts"
 
     for post in site.posts:
-        build_post(post, site, dist_dir)
+        build_post(post, site, dist_dir, env)
 
-def build_site(site: Site, dist_dir: Path) -> None:
 
-    build_pages(site, dist_dir)
-    build_posts(site, dist_dir)
-    build_atom_feed(site, dist_dir)
+def build_site(site: Site, dist_dir: Path, env: Environment) -> None:
+    build_pages(site, dist_dir, env)
+    build_posts(site, dist_dir, env)
+    build_atom_feed(site, dist_dir, env)
--- src/jssg/commands/build.py
+++ src/jssg/commands/build.py
@@ -3,6 +3,8 @@ from importlib import import_module
 from pathlib import Path
 from shutil import rmtree
 
+from jinja2 import Environment, PackageLoader
+
 from jssg.builder import build_site
 from jssg.command import BaseCommand
 from jssg.loaders import load_site
@@ -11,7 +13,13 @@ from jssg.settings import DIST_DIR
 
 class Command(BaseCommand):
     def add_arguments(self, parser: ArgumentParser) -> None:
-        parser.add_argument("-m", "--module", type=str, default="jtremesay", help="Specify the module to build")
+        parser.add_argument(
+            "-m",
+            "--module",
+            type=str,
+            default="jtremesay",
+            help="Specify the module to build",
+        )
 
     def handle(self, args: Namespace) -> None:
         dist_dir = Path(DIST_DIR)
@@ -19,4 +27,5 @@ class Command(BaseCommand):
 
         module = import_module(args.module)
         site = load_site(module)
-        build_site(site, dist_dir=dist_dir)
+        env = Environment(loader=PackageLoader(args.module))
+        build_site(site, dist_dir, env)
