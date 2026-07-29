36d89defa1e0cf6e8f1bd2d4ca29b6026b626029
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-07-29 14:37:16 +0200

    add license

--- src/jssg/management/commands/sandbox.py
+++ src/jssg/management/commands/sandbox.py
@@ -13,3 +13,12 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Run the sandbox command"
+
+    def handle(self, *args, **options):
+        pass
