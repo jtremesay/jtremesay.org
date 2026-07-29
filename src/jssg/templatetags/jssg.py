c966c3a8d492f1266eccaac50509b7664c917627
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-07-29 17:35:15 +0200

    check if static file exisits, template support fort md contnt

--- src/jssg/templatetags/jssg.py
+++ src/jssg/templatetags/jssg.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from django import template
+from django.contrib.staticfiles import finders
+from django.template.defaultfilters import stringfilter
+from django.templatetags.static import static
 from django.urls import reverse
 
 from jssg.models import Page
@@ -22,13 +25,41 @@ register = template.Library()
 
 
 @register.simple_tag
+@stringfilter
 def page_url(url: str) -> str:
     """
     Returns the URL of a page given its title.
     """
+    # Check if the page exists
     try:
         page = Page.objects.get(url=url)
     except Page.DoesNotExist:
         raise ValueError(f"Page with url '{url}' does not exist.")
 
+    # Could probably just use the input url
     return reverse("page", args=[page.url])
+
+
+@register.simple_tag
+@stringfilter
+def static_url(path: str) -> str:
+    """
+    Returns the URL of a static file given its path.
+    """
+    if not finders.find(path):
+        raise ValueError(f"Static file '{path}' does not exist.")
+
+    url = static(path)
+
+    return url
+
+
+@register.filter
+@stringfilter
+def content(value: str) -> str:
+    """
+    Renders a string as a Django template.
+    """
+    t = template.Template(value)
+    c = template.Context()
+    return t.render(c)
