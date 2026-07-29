6ab1f812a1f61e98b5f8b02b2505314575fa85e5
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-07-29 23:29:26 +0200

    simplify

--- website/front/raycaster/scene.ts
+++ website/front/raycaster/scene.ts
@@ -21,19 +21,6 @@
 import { Vector2 } from "./vector2"
 import { rect_sdf, union_sdf, differance_sdf } from "./sdf";
 
-/**
- * @brief Get the distance to the nearest object
- * @param point The point
- * @returns the distance between the point and the nearest object or Infinity if no objects
- */
-
-class Door {
-    center: Vector2;
-    constructor(center: Vector2) {
-        this.center = center;
-    }
-}
-
 class Room {
     center: Vector2;
     size: Vector2;
@@ -44,26 +31,27 @@ class Room {
     }
 }
 
+const DOOR_SIZE = new Vector2(1.1, 1.1);
+
 export class Scene {
     rooms: Room[];
-    doors: Door[];
 
     constructor() {
         this.rooms = []
-        this.doors = []
     }
 
-    add_room(center: Vector2, size: Vector2) {
+    add_room(center: Vector2, size: Vector2): Room {
         let room = new Room(center, size);
         this.rooms.push(room);
+
+        return room;
     }
 
-    add_door(center: Vector2) {
-        let door = new Door(center);
-        this.doors.push(door);
+    add_door(center: Vector2): Room {
+        return this.add_room(center, DOOR_SIZE);
     }
 
-    distance_to_neareast_object(point: Vector2) {
+    distance_to_neareast_object(point: Vector2): number {
         // Create an empty world
         let world = Infinity;
 
@@ -71,13 +59,6 @@ export class Scene {
         for (let room of this.rooms) {
             let room_sdf = rect_sdf(room.center, room.size, point);
             world = union_sdf(world, room_sdf);
-        }
-
-        // Add the doors
-        let door_size = new Vector2(1.1, 1.1);
-        for (let door of this.doors) {
-            let door_sdf = rect_sdf(door.center, door_size, point);
-            world = union_sdf(world, door_sdf);
         }
 
         // Do the magic!
